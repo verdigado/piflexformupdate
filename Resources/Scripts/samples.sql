c1aa8c8f7f90851666d53a85d06fcb59d4433b6c
WWW Dev <www-dev@dev-gl.gcms.verdigado.net>
2023-09-06 18:12:12 +0200

    [TASK] Update wizard for settings.flexform.pi.image.imageSizemode* #4380

--- Resources/Scripts/samples.sql
+++ Resources/Scripts/samples.sql
@@ -1,21 +1,14 @@
 SELECT    COUNT(uid),
           ExtractValue(pi_flexform,'/T3FlexForms/data/sheet[@index="image"]/language/field[@index="settings.flexform.pi.image.imageSizemodeList"]/value') AS imageSizemodeList
 FROM      tt_content
-WHERE     deleted=0 
-AND       hidden=0
-AND       CType = 'list'
+WHERE     CType = 'list'
+AND       deleted=0
+-- AND       hidden=0
 AND       list_type = 'xblog_pi1'
-AND       ExtractValue(pi_flexform,'/T3FlexForms/data/sheet[@index="image"]/language/field[@index="settings.flexform.pi.image.imageSizemodeList"]/value') = ''
+AND       (
+                ExtractValue(pi_flexform,'/T3FlexForms/data/sheet[@index="image"]/language/field[@index="settings.flexform.pi.image.imageSizemodeList"]/value') IS NULL
+            OR  ExtractValue(pi_flexform,'/T3FlexForms/data/sheet[@index="image"]/language/field[@index="settings.flexform.pi.image.imageSizemodeList"]/value') = ""
+            OR  ExtractValue(pi_flexform,'/T3FlexForms/data/sheet[@index="image"]/language/field[@index="settings.flexform.pi.image.imageSizemodeSingle"]/value') IS NULL
+            OR  ExtractValue(pi_flexform,'/T3FlexForms/data/sheet[@index="image"]/language/field[@index="settings.flexform.pi.image.imageSizemodeSingle"]/value') = ""
+          )
 GROUP BY  imageSizemodeList;
-
-
-SELECT    COUNT(uid),
-          ExtractValue(pi_flexform,'/T3FlexForms/data/sheet[@index="image"]/language/field[@index="settings.flexform.pi.image.imageSizemodeSingle"]/value') AS imageSizemodeSingle
-FROM      tt_content
-WHERE     deleted=0 
-AND       hidden=0
-AND       CType = 'list'
-AND       list_type = 'xblog_pi1'
-AND       ExtractValue(pi_flexform,'/T3FlexForms/data/sheet[@index="image"]/language/field[@index="settings.flexform.pi.image.imageSizemodeSingle"]/value') = ''
-GROUP BY  imageSizemodeSingle;
-
