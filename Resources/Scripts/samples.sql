d6b74b69d7666e246c2fc51fa4b3c447b6e622f6
Dirk Wildt <wildt@die-netzmacher.de>
2023-09-06 15:46:46 +0200

    [FEATURE] Update wizard for settings.flexform.pi.image.imageSizemodeList and settings.flexform.pi.image.imageSizemodeSingle #4380

---
+++ Resources/Scripts/samples.sql
@@ -0,0 +1,19 @@
+SELECT    COUNT(uid),
+          ExtractValue(pi_flexform,'/T3FlexForms/data/sheet[@index="image"]/language/field[@index="settings.flexform.pi.image.imageSizemodeList"]/value') AS imageSizemodeList
+FROM      tt_content
+WHERE     deleted=0 
+AND       hidden=0
+AND       CType = 'list'
+AND       list_type = 'xblog_pi1'
+GROUP BY  imageSizemodeList;
+
+
+SELECT    COUNT(uid),
+          ExtractValue(pi_flexform,'/T3FlexForms/data/sheet[@index="image"]/language/field[@index="settings.flexform.pi.image.imageSizemodeSingle"]/value') AS imageSizemodeSingle
+FROM      tt_content
+WHERE     deleted=0 
+AND       hidden=0
+AND       CType = 'list'
+AND       list_type = 'xblog_pi1'
+GROUP BY  imageSizemodeSingle;
+
